62e83dec92a8a08b1ce3fe94a334e933b70bd4f5
LuichiTW <ltitaewatanabe@frba.utn.edu.ar>
2025-05-17 19:20:28 -0300

    ADD: movimiento cinta y ruedas

--- TGC.MonoGame.TP/src/Graficos/Modelos/MTanque.cs
+++ TGC.MonoGame.TP/src/Graficos/Modelos/MTanque.cs
@@ -16,8 +16,12 @@ namespace TGC.MonoGame.TP.src.Tanques
 
         //----------------------------------------------Variables--------------------------------------------------//
         private TipoTanque _tipoTanque;
+        private Effect effectRueda;
         Texture2D tanqueTexture;
+        Texture2D texturaCinta;
         float giroTorrreta = 5f;
+        float rotacion = 0f;
+        float offsetCinta = 0f;
         string[] meshes;
 
         //----------------------------------------------Constructores-e-inicializador--------------------------------------------------//
@@ -37,6 +41,8 @@ namespace TGC.MonoGame.TP.src.Tanques
         {
             this._modelo = Content.Load<Model>("Models/tgc-tanks" + this._tipoTanque.directorioModelo());
             tanqueTexture = Content.Load<Texture2D>("Models/tgc-tanks" + this._tipoTanque.directorioTextura());
+            texturaCinta = Content.Load<Texture2D>("Models/tgc-tanks/T90/textures_mod/treadmills");
+            effectRueda = Content.Load<Effect>("Effects/shaderRuedas");
             //obtenemos los meshes del modelo
             int count = 0;
             int meshCount = _modelo.Meshes.Count;
@@ -53,7 +59,7 @@ namespace TGC.MonoGame.TP.src.Tanques
                     // Asignar nombre genérico si no tiene
                     mesh.Name = $"Mesh_{count}";
                     meshes[count] = mesh.Name;
-                    Console.WriteLine($"Mesh {count}: {mesh.Name}");
+                    //Console.WriteLine($"Mesh {count}: {mesh.Name}");
                 }
                 count++;
             }
@@ -62,7 +68,7 @@ namespace TGC.MonoGame.TP.src.Tanques
 
         protected override void AjustarModelo()
         {
-            this._matixBase = Matrix.CreateScale(this._tipoTanque.escala()) * Matrix.CreateRotationX(this._tipoTanque.angulo()) * Matrix.CreateRotationY(MathF.PI);//TODO - SOLO ROTA EN X. Si queres hacerlo hermoso, cambiar. Escala de modelo no amerita.
+            this._matixBase = Matrix.CreateScale(this._tipoTanque.escala()) * Matrix.CreateRotationX(this._tipoTanque.angulo()) * Matrix.CreateRotationY(MathF.PI + 2f);//TODO - SOLO ROTA EN X. Si queres hacerlo hermoso, cambiar. Escala de modelo no amerita.
         }
 
         //----------------------------------------------Dibujado--------------------------------------------------//
@@ -74,12 +80,49 @@ namespace TGC.MonoGame.TP.src.Tanques
             _effect2.Parameters["World"].SetValue(this._matrixMundo);
             _effect2.Parameters["Texture"].SetValue(tanqueTexture);
 
+            effectRueda.Parameters["View"].SetValue(this._matrixView);
+            effectRueda.Parameters["Projection"].SetValue(this._matrixProyection);
+            effectRueda.Parameters["World"].SetValue(this._matrixMundo);
+            effectRueda.Parameters["Texture"].SetValue(tanqueTexture);
+
+            // prueba de rotacion de ruedas TODO crear funcion
+            rotacion -= 0.1f;
+
+            //prueba de movimiento de la cinta
+            offsetCinta += 0.01f;
+            if (offsetCinta > 1.0f)
+                offsetCinta -= 1.0f;
+
+            //TODO mejorar la lectura del codigo
             foreach (var mesh in _modelo.Meshes)
             {
-                if (mesh.Name == "Turret" || mesh.Name == "Cannon")
+                if (mesh.Name.Contains("Tread"))
+                {
+                    effectRueda.Parameters["UVOffset"].SetValue(new Vector2(0, offsetCinta));
+                    effectRueda.Parameters["Texture"].SetValue(texturaCinta);
+                    foreach (ModelMeshPart part in mesh.MeshParts)
+                    {
+                        part.Effect = effectRueda;
+                    }
+
+                    effectRueda.Parameters["World"].SetValue(mesh.ParentBone.Transform * _matrixMundo);
+
+                }
+                else if (mesh.Name == "Turret" || mesh.Name == "Cannon")
                 {
                     //posiblemente de problemas al calcular con las normales del mapa
                     _effect2.Parameters["World"].SetValue(mesh.ParentBone.Transform * Matrix.CreateRotationZ(giroTorrreta) * _matrixMundo);
+                }
+                else if (mesh.Name.Contains("Wheel"))
+                {
+                    Vector3 wheelCenter = mesh.BoundingSphere.Center;
+
+                    Matrix transform = mesh.ParentBone.Transform;
+                    transform = Matrix.CreateTranslation(-wheelCenter) *
+                              Matrix.CreateRotationX(rotacion) * // Rotación en X
+                              Matrix.CreateTranslation(wheelCenter) *
+                              transform;
+                    _effect2.Parameters["World"].SetValue(transform * _matrixMundo);
                 }
                 else
                 {
@@ -95,6 +138,7 @@ namespace TGC.MonoGame.TP.src.Tanques
             return Content.Load<Effect>("Effects/shaderT90");
         }
 
+        // ajustar con delta time
         public void rotarTorreta(float giro)
         {
             giroTorrreta += giro;
