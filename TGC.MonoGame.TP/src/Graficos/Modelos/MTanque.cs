aeeab03831f0dc88a2cb693185cbd8d6caf32bbb
LuichiTW <ltitaewatanabe@frba.utn.edu.ar>
2025-05-17 22:19:13 -0300

    ADD: animacion dependiendo del movimiento

--- TGC.MonoGame.TP/src/Graficos/Modelos/MTanque.cs
+++ TGC.MonoGame.TP/src/Graficos/Modelos/MTanque.cs
@@ -20,8 +20,8 @@ namespace TGC.MonoGame.TP.src.Tanques
         Texture2D tanqueTexture;
         Texture2D texturaCinta;
         float giroTorrreta = 5f;
-        float rotacion = 0f;
-        float offsetCinta = 0f;
+        Vector2 rotacionRuedas = new Vector2(0f, 0f);
+        Vector2 offsetCintas = new Vector2(0f, 0f);
         string[] meshes;
 
         //----------------------------------------------Constructores-e-inicializador--------------------------------------------------//
@@ -43,6 +43,8 @@ namespace TGC.MonoGame.TP.src.Tanques
             tanqueTexture = Content.Load<Texture2D>("Models/tgc-tanks" + this._tipoTanque.directorioTextura());
             texturaCinta = Content.Load<Texture2D>("Models/tgc-tanks/T90/textures_mod/treadmills");
             effectRueda = Content.Load<Effect>("Effects/shaderRuedas");
+            offsetCintas = getAnimacionTanque(0, 0.01f); // esta es la animacion por defecto, 0 = detenido
+            rotacionRuedas = getAnimacionTanque(0, 0.1f); // esta es la animacion por defecto, 0 = detenido
             //obtenemos los meshes del modelo
             int count = 0;
             int meshCount = _modelo.Meshes.Count;
@@ -52,7 +54,7 @@ namespace TGC.MonoGame.TP.src.Tanques
                 if (!string.IsNullOrEmpty(mesh.Name))
                 {
                     meshes[count] = mesh.Name;
-                    Console.WriteLine($"Mesh {count}: {mesh.Name}");
+                    //Console.WriteLine($"Mesh {count}: {mesh.Name}");
                 }
                 else
                 {
@@ -85,20 +87,34 @@ namespace TGC.MonoGame.TP.src.Tanques
             effectRueda.Parameters["World"].SetValue(this._matrixMundo);
             effectRueda.Parameters["Texture"].SetValue(tanqueTexture);
 
+            //---------------------------- variables prueba--------------------------------------------------------
+            /*
             // prueba de rotacion de ruedas TODO crear funcion
-            rotacion -= 0.1f;
+            rotacionRuedas += new Vector2(1, 1) * 0.1f;
 
             //prueba de movimiento de la cinta
-            offsetCinta += 0.01f;
-            if (offsetCinta > 1.0f)
-                offsetCinta -= 1.0f;
+            offsetCintas.X += 0.01f;
+            if (offsetCintas.X > 1.0f)
+                offsetCintas.X -= 1.0f;
 
+            offsetCintas.Y = -offsetCintas.X;
+
+            */
+            //------------------------------dibujado de los meshes---------------------------------------------------
             //TODO mejorar la lectura del codigo
             foreach (var mesh in _modelo.Meshes)
             {
-                if (mesh.Name.Contains("Tread"))
+                if (mesh.Name.Contains("Treadmill"))
                 {
-                    effectRueda.Parameters["UVOffset"].SetValue(new Vector2(0, offsetCinta));
+                    //desplazamiento de la cinta individual
+                    if (mesh.Name.Contains("1"))
+                    {
+                        effectRueda.Parameters["UVOffset"].SetValue(new Vector2(0, offsetCintas.X));
+                    }
+                    else
+                    {
+                        effectRueda.Parameters["UVOffset"].SetValue(new Vector2(0, offsetCintas.Y));
+                    }
                     effectRueda.Parameters["Texture"].SetValue(texturaCinta);
                     foreach (ModelMeshPart part in mesh.MeshParts)
                     {
@@ -117,11 +133,24 @@ namespace TGC.MonoGame.TP.src.Tanques
                 {
                     Vector3 wheelCenter = mesh.BoundingSphere.Center;
 
-                    Matrix transform = mesh.ParentBone.Transform;
-                    transform = Matrix.CreateTranslation(-wheelCenter) *
-                              Matrix.CreateRotationX(rotacion) * // Rotación en X
-                              Matrix.CreateTranslation(wheelCenter) *
-                              transform;
+                    Matrix transform = mesh.ParentBone.Transform * Matrix.CreateRotationZ(MathF.PI);
+
+                    //giro individual de cada rueda
+                    if (esRuedaDerecha(mesh.Name))
+                    {
+                        transform = Matrix.CreateTranslation(-wheelCenter) *
+                          Matrix.CreateRotationX(rotacionRuedas.Y) * // Rotación en X
+                          Matrix.CreateTranslation(wheelCenter) *
+                          transform;
+                    }
+                    else
+                    {
+                        transform = Matrix.CreateTranslation(-wheelCenter) *
+                          Matrix.CreateRotationX(rotacionRuedas.X) * // Rotación en X
+                          Matrix.CreateTranslation(wheelCenter) *
+                          transform;
+                    }
+
                     _effect2.Parameters["World"].SetValue(transform * _matrixMundo);
                 }
                 else
@@ -152,5 +181,50 @@ namespace TGC.MonoGame.TP.src.Tanques
             }
         }
 
+        // 0 detenido
+        // 1 marcha adelante
+        // 2 marcha atras
+        // 3 giro a la derecha
+        // 4 giro a la izquierda
+        public Vector2 getAnimacionTanque(int animacion, float velocidad)
+        {
+            if (animacion == 0)
+            {
+                return new Vector2(0, 0);
+            }
+            else if (animacion == 1)
+            {
+                return new Vector2(1, 1) * velocidad;
+            }
+            else if (animacion == 2)
+            {
+                return new Vector2(-1, -1) * velocidad;
+            }
+            else if (animacion == 3)
+            {
+                return new Vector2(1, -1) * velocidad;
+            }
+            else if (animacion == 4)
+            {
+                return new Vector2(-1, 1) * velocidad;
+            }
+            else
+            {
+                return new Vector2(0, 0);
+            }
+        }
+
+        public bool esRuedaDerecha(string nombre)
+        {
+            if (nombre.Contains("9") || nombre.Contains("10") || nombre.Contains("11") || nombre.Contains("12") || nombre.Contains("13") || nombre.Contains("14") || nombre.Contains("15") || nombre.Contains("16"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
     }
 }
